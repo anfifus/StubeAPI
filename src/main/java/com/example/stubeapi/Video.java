4b177d3f8a989ff380234a60dba23e16de986500
Aleix Alvarez <anfifus@gmail.com>
2022-03-23 11:35:50 +0100

    commit initial

--- src/main/java/com/example/stubeapi/Video.java
+++ src/main/java/com/example/stubeapi/Video.java
@@ -1,10 +1,15 @@
 package com.example.stubeapi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Video {
+    private long uuidVideo = UUID.randomUUID().getMostSignificantBits();
     private String urlVideo;
     private String title;
     private String description;
-
+    private List<Score> scores = new ArrayList<>();
     public Video() {
     }
 
@@ -23,7 +28,7 @@ public class Video {
     }
 
     private void checkUrlVideo(String urlVideo) throws Exception{
-        String initUrl = urlVideo.substring(0,3);
+        String initUrl = urlVideo.substring(0,4);
         if(!checkPattern(initUrl)){
             throw new Exception("The pattern of www. in the beginning doesn't match in this case");
         }
@@ -31,6 +36,10 @@ public class Video {
 
     private boolean checkPattern(String initUrl) {
         return initUrl.equals("www.");
+    }
+
+    public long getUuidVideo() {
+        return uuidVideo;
     }
 
     public String getUrlVideo() {
@@ -44,4 +53,27 @@ public class Video {
     public String getDescription() {
         return description;
     }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setUrlVideo(String urlVideo) throws Exception {
+        checkUrlVideo(urlVideo);
+        this.urlVideo = urlVideo;
+    }
+
+    public void setTitle(String title) throws Exception {
+        checkTitle(title);
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void addScores(Score score) {
+        this.scores.add(score);
+    }
+
 }
